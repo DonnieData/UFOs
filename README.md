2b04282d0d66157ae613d43edcbae6557796f0ee
Donnie <Donnell.Jackson@outlook.com>
2020-12-16 16:28:38 -0600

    adding files to repo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UFOs
 Building a dynamic webpage to filter and display data  using JavaScript.
+
+## Resources 
+
+
+## Overview 
+
+
+## Processes 
+
+
+## Summary 
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+// import the data from data.js
+const tableData = data;
+
+// referrence the HTML table using d3
+// use d3 to look for the <tbody> tags in html 
+var tbody = d3.select("tbody");
