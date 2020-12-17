7c0f853a755841181db5b11784165c793ee6d6a8
Donnie <Donnell.Jackson@outlook.com>
2020-12-16 23:33:16 -0600

    building javascript script to create table for date

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,22 @@ const tableData = data;
 // use d3 to look for the <tbody> tags in html 
 var tbody = d3.select("tbody");
 
+function buildTable(data) {
+    // clear data - start with clean/empty table
+    tbody.html("");
+
+
+    //dataRow represents each object(row)
+    data.forEach((dataRow) => {
+        // find <tbody> and add new row -- each object row will be wrapped in a <tr> tag
+        let row = tbody.append("tr");
+    
+        // loop through each field in dataRow and appending to table, wrapped in <td> tags
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            // holds only each vlaue from object 
+            cell.text(val); 
+            }
+        );
+    });
+}
